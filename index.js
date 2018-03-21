44798fcc3b071331b10ea9b32e7c4aeb31d06853
Szu <szuscik.michal@gmail.com>
2018-03-21 13:40:02 +0100

    change console notification to every 5 minutes; add posibility to define time interval and message by console

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 const notifier = require('node-notifier');
-const minutesNumber = 30;
+
+const defaultMinutesNumber = 30;
+const defaultMessage = "It's time to quick break!";
+
+const args = process.argv.slice(2);
+const minutesNumber = parseInt(args[0]) || defaultMinutesNumber;
+const message = args[1] || defaultMessage;
 
 console.log('Notifier started.');
 
 let index = minutesNumber;
-
 setInterval(() => {
-    console.log(`${--index} minutes left to break.`);
+  --index;
+  if (index % 5 == 0) console.log(`${index} minutes left to break.`);
 
-    if (index < 1) {
-        notifier.notify({
-            title: 'Break!!',
-            message: 'It\'s time to quick break!'
-        });
-        index = minutesNumber;
-    }
+  if (index < 1) {
+    notifier.notify({
+      title: 'Break!!',
+      message: message
+    });
+    index = minutesNumber;
+
+    console.log('Break notification', Date());
+  }
 }, 60 * 1000);
